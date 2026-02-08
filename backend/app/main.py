9a675e6998b7d60c0094e0019c237b11c1d90ee2
laxmanrathore01 <laxmansajjan2004@gmail.com>
2026-02-09 02:26:13 +0530

    Prepare backend for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-from app.routes import employee, attendance
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"message": "HRMS Lite Backend Running 🚀"}
-
-app.include_router(employee.router, prefix="/employees", tags=["Employees"])
-app.include_router(attendance.router, prefix="/attendance", tags=["Attendance"])
